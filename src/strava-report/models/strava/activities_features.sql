15f2b3e0bdf08c55d5be93858ff424589fef926b
Serge Bouschet <serge.bouschet@gmail.com>
2022-01-26 21:39:26 +0000

    new incremental model

---
+++ src/strava-report/models/strava/activities_features.sql
@@ -0,0 +1,45 @@
+{{ config(materialized='incremental',
+          unique_key='id',
+          incremental_strategy='insert_overwrite',
+          partition_by={
+             "field": "start_date",
+             "data_type": "datetime",
+             "granularity": "day"
+          },
+          enabled=True)
+}}
+
+with source_data as (
+  select
+    id,
+    name,
+    round(average_speed*3.6, 1)     as avg_speed,
+    average_speed,
+    average_heartrate,
+    average_watts,
+    round(distance/1000, 1)    as distance_km,
+    elapsed_time,
+    safe.parse_datetime('%Y-%m-%dT%H:%M:%S', start_date_local) as start_date,
+    EXTRACT(year from safe.parse_datetime('%Y-%m-%dT%H:%M:%S', start_date_local)) as start_year,
+    EXTRACT(month from safe.parse_datetime('%Y-%m-%dT%H:%M:%S', start_date_local)) as start_month,
+    EXTRACT(dayofweek from safe.parse_datetime('%Y-%m-%dT%H:%M:%S', start_date_local)) as start_day,
+    start_date_local,
+    round(total_elevation_gain, 0) as total_elevation_gain,
+    type,
+    kudos_count,
+    gear_id,
+    average_temp,
+    moving_time,
+    photo_count,
+    athlete_count,
+    comment_count,
+    description
+  from {{ source('strava', 'new_activities') }} activities
+)
+
+select
+     activities.*,
+     gear.bike
+from source_data as activities
+left join {{ ref('strava_gear') }} gear
+     on activities.gear_id = gear.id
